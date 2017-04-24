6f932faadc4edce951f65c68970fe088437b3319
Tin Vo <tinnvo1101@gmail.com>
2017-04-24 13:28:29 -0500

    Added SASS

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -40,6 +40,16 @@ module.exports = {
         loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
       },
       {
+          test: /\.scss$/,
+              use: [{
+                  loader: "style-loader" // creates style nodes from JS strings
+              }, {
+                  loader: "css-loader" // translates CSS into CommonJS
+              }, {
+                  loader: "sass-loader" // compiles Sass to CSS
+              }]
+      },
+      {
         test: /\.css$/,
         include: helpers.root('src', 'app'),
         loader: 'raw-loader'
