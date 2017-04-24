c0ea17fa3243cc5ae6e6a63090b6fdb7b21732a5
Tin Vo <tinnvo1101@gmail.com>
2017-04-24 11:54:52 -0500

    Added favicon

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 var helpers = require('./helpers');
 
 module.exports = {
@@ -63,7 +64,7 @@ module.exports = {
     new webpack.optimize.CommonsChunkPlugin({
       name: ['app', 'vendor', 'polyfills']
     }),
-
+    new FaviconsWebpackPlugin('./src/favicon.ico'),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     })
