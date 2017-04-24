a3ed6fa63c14a1ab4e9c90bdf4f0a41f370b0247
Tin Vo <tinnvo1101@gmail.com>
2017-04-24 08:54:23 -0500

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-
+import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SongComponent } from './components/song/song.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+
+const routes : Routes = [
+    { path: '', redirectTo: 'search', pathMatch: 'full'},
+    { path: 'search', component: HomeComponent},
+    { path: 'tracks/:id', component: SongComponent},
+    { path: 'albums/:id', component: PlaylistComponent},
+    { path: 'contactus', redirectTo: 'contact'},
+];
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SongComponent,
+    PlaylistComponent,
+
   ],
   bootstrap: [ AppComponent ]
 })
